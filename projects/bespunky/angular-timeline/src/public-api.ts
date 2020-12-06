57c04878edca879f90ac5400c7878ae79bf13065
Shy Agam <us@bespunky.io>
2020-12-06 17:42:09 +0200

    ➖ Removed the generated src folder from the library

--- projects/bespunky/angular-timeline/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of angular-timeline
- */
-
-export * from './lib/angular-timeline.service';
-export * from './lib/angular-timeline.component';
-export * from './lib/angular-timeline.module';
