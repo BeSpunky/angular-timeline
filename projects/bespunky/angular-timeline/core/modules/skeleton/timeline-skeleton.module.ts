b3b6c7cbfb8179b89014f935aadd23f411d50540
Shy Agam <us@bespunky.io>
2020-12-09 15:35:09 +0200

    ➖ Removed template component
    Replaced by directive

--- projects/bespunky/angular-timeline/core/modules/skeleton/timeline-skeleton.module.ts
+++ projects/bespunky/angular-timeline/core/modules/skeleton/timeline-skeleton.module.ts
@@ -1,10 +1,11 @@
 import { NgModule     } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { TimelineRangeDirective } from './directives/timeline-range.directive';
+
 @NgModule({
-    imports: [
-        CommonModule
-    ],
-    declarations: []
+    imports     : [CommonModule],
+    declarations: [TimelineRangeDirective],
+    exports     : [TimelineRangeDirective]
 })
 export class TimelineSkeletonModule { }
