850ef95c37a74d9be18add52c420c8a5581685d6
Shy Agam <us@bespunky.io>
2020-12-17 10:08:40 +0200

    ➕ Exposed timeline module as public api

---
+++ projects/bespunky/angular-timeline/core/public-api.ts
@@ -0,0 +1,9 @@
+/*
+ * Public API Surface of angular-timeline
+ */
+
+export * from './timeline.module';
+export * from './timeline.component';
+
+export * from './modules/skeleton/timeline-skeleton.module';
+export * from './modules/skeleton/directives/timeline-range.directive';
--- projects/bespunky/angular-timeline/core/timeline.module.ts
+++ projects/bespunky/angular-timeline/core/timeline.module.ts
@@ -2,12 +2,14 @@ import { NgModule     } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TimelineSkeletonModule } from './modules/skeleton/timeline-skeleton.module';
+import { TimelineComponent } from './timeline.component';
 
 @NgModule({
     imports: [
         CommonModule,
         TimelineSkeletonModule
     ],
-    declarations: []
+    declarations: [TimelineComponent],
+    exports     : [TimelineComponent]
 })
 export class TimelineModule { }
