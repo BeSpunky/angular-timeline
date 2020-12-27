8ba64ca7001ad7460c58238b69150e468fe92bba
Shy Agam <us@bespunky.io>
2020-12-27 13:41:11 +0200

    🔧 Exposed ticks renderer component as public api

--- projects/bespunky/angular-timeline/core/public-api.ts
+++ projects/bespunky/angular-timeline/core/public-api.ts
@@ -7,3 +7,4 @@ export * from './timeline.component';
 
 export * from './modules/skeleton/timeline-skeleton.module';
 export * from './modules/skeleton/directives/timeline-ticks-definition.directive';
+export * from './modules/skeleton/components/timeline-ticks/timeline-ticks.component';
