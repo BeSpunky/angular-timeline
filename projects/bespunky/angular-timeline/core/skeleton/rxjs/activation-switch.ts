075c7732d79d0991f0ba96659d400c5d81175158
Shy Agam <us@bespunky.io>
2021-02-12 18:54:13 +0200

    ⚡ Optimized activation switch operator by only notifying on value changes

--- projects/bespunky/angular-timeline/core/skeleton/rxjs/activation-switch.ts
+++ projects/bespunky/angular-timeline/core/skeleton/rxjs/activation-switch.ts
@@ -1,10 +1,11 @@
-import { Observable, OperatorFunction, pipe, UnaryFunction } from 'rxjs';
-import { filter, mergeMap, windowToggle } from 'rxjs/operators';
+import { Observable, pipe, UnaryFunction } from 'rxjs';
+import { distinctUntilChanged, filter, mergeMap, windowToggle } from 'rxjs/operators';
 
 export function useActivationSwitch<T>(observable: Observable<boolean>): UnaryFunction<Observable<T>, Observable<T>>
 {
-    const on  = observable.pipe(filter(activate => activate));
-    const off = observable.pipe(filter(activate => !activate));
+    const activate = observable.pipe(distinctUntilChanged());
+    const on       = activate.pipe(filter(activate => activate));
+    const off      = activate.pipe(filter(activate => !activate));
     
     return pipe(
         windowToggle<T, boolean>(on, () => off),
