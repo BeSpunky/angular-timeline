027d487db1ccb3be096ead2af9f2b50a46a8b652
Shy Agam <us@bespunky.io>
2021-02-14 19:50:08 +0200

    🔬🐛 Fixed virtualization calculations are incorrect

--- projects/bespunky/angular-timeline/core/skeleton/dynamic-tick-items-reverese.prototype.ts
+++ projects/bespunky/angular-timeline/core/skeleton/dynamic-tick-items-reverese.prototype.ts
@@ -1,12 +1,12 @@
-import { subYears } from 'date-fns';
+import { eachYearOfInterval, eachMonthOfInterval, eachDayOfInterval, eachHourOfInterval, subYears, differenceInMinutes, addMinutes } from 'date-fns';
 
 (function dynamicTickItemsReverse()
 {
-    const date = (y: number, m: number = 0, d: number = 1, h: number = 0, mm: number = 0, s: number = 0, ms: number = 0) =>
+    const date = (yOrDate: number | Date, m: number = 0, d: number = 1, h: number = 0, mm: number = 0, s: number = 0, ms: number = 0) =>
     {
-        let x = new Date(Date.UTC(y, m, d, h, mm, s, ms));
+        let x = yOrDate instanceof Date ? yOrDate : new Date(Date.UTC(yOrDate, m, d, h, mm, s, ms));
         
-        if (0 <= y && y <= 99)
+        if (0 <= yOrDate && yOrDate <= 99)
             return subYears(x, 1900);
             
         return x;
@@ -35,7 +35,7 @@ import { subYears } from 'date-fns';
         va;
     }
 
-    const daysSinceZero = (y: number, m: number = 0, d: number = 1, h: number = 0, mm: number = 0, s: number = 0, ms: number = 0) => ((date(y, m, d, h, mm, s, ms).getTime() - zero) / 1000 / 60 / 60 / 24);
+    const daysSinceZero = (yOrDate: number | Date, m: number = 0, d: number = 1, h: number = 0, mm: number = 0, s: number = 0, ms: number = 0) => ((date(yOrDate, m, d, h, mm, s, ms).getTime() - zero) / 1000 / 60 / 60 / 24);
     const daysInMonth   = (y: number, m: number) => new Date(y, m + 1, 0).getDate();
     const daysInYear    = (y: number) => y % 400 === 0 || (y % 100 !== 0 && y % 4 === 0) ? 366 : 365;
 
@@ -54,11 +54,7 @@ import { subYears } from 'date-fns';
     const monthWidth  = (y: number, m: number) => dayWidth * daysInMonth(y, m);
     const yearWidth   = (y: number)            => dayWidth * daysInYear(y);
 
-    const minutePosition = (y: number, m: number, d: number, hr: number, min: number) => dayWidth * daysSinceZero(y, m, d, hr, min);
-    const hourPosition   = (y: number, m: number, d: number, hr: number)              => dayWidth * daysSinceZero(y, m, d, hr);
-    const dayPosition    = (y: number, m: number, d: number)                          => dayWidth * daysSinceZero(y, m, d);
-    const monthPosition  = (y: number, m: number)                                     => dayWidth * daysSinceZero(y, m);
-    const yearPosition   = (y: number)                                                => dayWidth * daysSinceZero(y);
+    const dateToPosition = (yOrDate: number | Date, m?: number, d?: number, h?: number, mm?: number, s?: number, ms?: number) => dayWidth * daysSinceZero(yOrDate, m, d, h, mm, s, ms);
 
     const positionToDate = (position: number) => new Date(new Date(position * 24 * 60 * 60 * 1000 / dayWidth).getTime() + zero);
     
@@ -67,52 +63,49 @@ import { subYears } from 'date-fns';
     const closestMinuteDaysFromZero = (position: number) =>
     {
         const date = positionToDate(position);
-        return minutePosition(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes());
+        return dateToPosition(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes());
     };
     const closestHourDaysFromZero   = (position: number) =>
     {
-        const date = positionToDate(position)/*? $.toISOString()*/;
-        return hourPosition(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours());
+        const date = positionToDate(position);
+        return dateToPosition(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours());
     };
     const closestDayDaysFromZero    = (position: number) =>
     {
         const date = positionToDate(position);
-        return dayPosition(date.getFullYear(), date.getMonth(), date.getDate());
+        return dateToPosition(date.getFullYear(), date.getMonth(), date.getDate());
     };
     const closestMonthDaysFromZero  = (position: number) =>
     {
         const date = positionToDate(position);
-        return monthPosition(date.getFullYear(), date.getMonth());
+        return dateToPosition(date.getFullYear(), date.getMonth());
     };
     const closestYearDaysFromZero   = (position: number) =>
     {
         const date = positionToDate(position);
-        return yearPosition(date.getFullYear());
+        return dateToPosition(date.getFullYear());
     };
     
-    const position = 60;
-
     const r = (x: number) => Math.round(x * 1000) / 1000;
 
     const testDateToPosition = (month: number) =>
     {
-
         r(yearWidth  (year));/*?*/
         r(monthWidth (year, month));/*?*/
         r(dayWidth   );/*?*/
         r(hourWidth  ());/*?*/
         r(minuteWidth());/*?*/
 
-        r(yearPosition  (year                     ));/*?*/
-        r(monthPosition (year, month              ));/*?*/
-        r(dayPosition   (year, month, day         ));/*?*/
-        r(hourPosition  (year, month, day, hr     ));/*?*/
-        r(minutePosition(year, month, day, hr, min));/*?*/
+        r(dateToPosition(year                     ));/*?*/
+        r(dateToPosition(year, month              ));/*?*/
+        r(dateToPosition(year, month, day         ));/*?*/
+        r(dateToPosition(year, month, day, hr     ));/*?*/
+        r(dateToPosition(year, month, day, hr, min));/*?*/
     };
 
-    const testPositionToDate = (position: number) =>
+    const testClosets = (position: number) =>
     {
-        positionToDate    (position)/*? $.toISOString()*/;
+        positionToDate             (position )/*? $.toISOString()*/;
         r(closestYearDaysFromZero  (position))/*?*/;
         r(closestMonthDaysFromZero (position))/*?*/;
         r(closestDayDaysFromZero   (position))/*?*/;
@@ -122,32 +115,26 @@ import { subYears } from 'date-fns';
 
     [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].forEach(month => testDateToPosition(month));
 
-    [-3, -2, -1, 0, 1, 2, 3].forEach(position => testPositionToDate(position));
-
-    const indexToPosition = (daysFromZero: number) => daysFromZero * dayWidth;
+    [-3, -2, -1, 0, 1, 2, 3].forEach(position => testClosets(position));
 
     const screenWidth  = 10;
-    const daysOnScreen = screenWidth / dayWidth;/*?*/
+    const daysOnScreen = screenWidth / dayWidth;      /*?*/
+    const position     = 5;
+    const left         = position - screenWidth / 2;  /*?*/
+    const right        = position + screenWidth / 2;  /*?*/
     
-    const generateIndexesOnScreen = (centerDayIndex: number) =>
+    // ! Buffer should be expressed in percentage of screen width. 50% of buffer means twice as much ticks (50% to the left, 50% to the right)
+    const virtualize = (each: (interval: Interval) => Date[]) =>
     {
-        const prefixCount = daysOnScreen / 2;
-
-        return Array.from({ length: daysOnScreen }, (_, i) => r(i - prefixCount  - centerDayIndex));
+        return each({
+            start: positionToDate(left)/*? */,
+            end  : positionToDate(right)/*? */
+        })/*? */.map(date => r(dateToPosition(date)));
     };
 
-    generateIndexesOnScreen(0);/*?*/
-
-    const testVirtualization = (position: number) =>
-    {
-        const virtualize = (closestTickDaysFromZero: number) => generateIndexesOnScreen(closestTickDaysFromZero).map(indexToPosition);
-
-        virtualize(closestYearDaysFromZero  (position)) /*?*/;
-        virtualize(closestMonthDaysFromZero (position)) /*?*/;
-        virtualize(closestDayDaysFromZero   (position)) /*?*/;
-        virtualize(closestHourDaysFromZero  (position)) /*?*/;
-        virtualize(closestMinuteDaysFromZero(position)) /*?*/;
-    };
-
-    testVirtualization(0);
+    virtualize(eachYearOfInterval); /*?*/
+    virtualize(eachMonthOfInterval);/*?*/
+    virtualize(eachDayOfInterval);  /*?*/
+    virtualize(eachHourOfInterval); /*?*/
+    virtualize(({ start, end }) => Array.from({ length: (differenceInMinutes(start, end)) }, (_, minIndex) => addMinutes(start, minIndex)));/*?*/
 })();
