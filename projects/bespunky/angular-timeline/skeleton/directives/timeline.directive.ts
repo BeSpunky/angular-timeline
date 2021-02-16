69b5ef0ce0158b2f11540ec913cef2b55196f9c4
Shy Agam <us@bespunky.io>
2021-02-16 20:26:51 +0200

    📃 Documented the timeline directive

--- projects/bespunky/angular-timeline/skeleton/directives/timeline.directive.ts
+++ projects/bespunky/angular-timeline/skeleton/directives/timeline.directive.ts
@@ -13,6 +13,14 @@ import { TimelineTickDirective } from '../modules/ticks/directives/timeline-tick
 import { TimelineTick } from '../modules/ticks/directives/timeline-tick';
 import { TimelineTickRenderer } from '../modules/ticks/services/render/timeline-tick-renderer';
 
+/**
+ * Adds timeline functionality to an element.
+ *
+ * @export
+ * @class TimelineDirective
+ * @extends {Destroyable}
+ * @implements {AfterViewInit}
+ */
 @Directive({
     selector : '[timeline]',
     exportAs : 'timeline',
@@ -20,10 +28,24 @@ import { TimelineTickRenderer } from '../modules/ticks/services/render/timeline-
 })
 export class TimelineDirective extends Destroyable implements AfterViewInit
 {
-    @ContentChildren(TimelineTickDirective) public ticks!: QueryList<TimelineTick>;
+    @ContentChildren(TimelineTickDirective) private ticks!: QueryList<TimelineTick>;
     
+    /**
+     * The view bounds currently focused on by the user represented in a format compatible with an SVG viewBox.
+     *
+     * @type {Observable<string>}
+     */
     public readonly svgViewBox: Observable<string>;
     
+    /**
+     * Creates an instance of TimelineDirective.
+     * 
+     * @param {ChangeDetectorRef} changes
+     * @param {TimelineState} state The state of the timeline.
+     * @param {TimelineControl} control
+     * @param {TimelineRenderer} renderer
+     * @param {TimelineTickRenderer} tickRenderer
+     */
     constructor(
         private changes     : ChangeDetectorRef,
         public  state       : TimelineState,
@@ -70,13 +92,19 @@ export class TimelineDirective extends Destroyable implements AfterViewInit
         this.subscribe(unrender, _             => this.tickRenderer.unrenderTicks(tickLevel));
     }
 
-    /** The width of the top level tick in zero-zoom mode. */
+    /**
+     * Sets the width of the the day level tick. All tick width and position calculations are based on this size.
+     * Default is 1. A larger size means the initial size of your ticks will be larger.    
+     */
     @Input() public set baseTickSize(value: number)
     {
         this.state.baseTickSize.next(value);
     }
 
-    /** The width of the top level tick in zero-zoom mode. */
+    /**
+     * The level of zoom to apply to when rendering the timeline. Default is 1.
+     * A larger number means zooming-in; A smaller number means zooming-out.
+     */
     @Input() public set zoom(value: number)
     {
         this.state.zoom.next(value);
