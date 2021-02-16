d342065d55d2c9c2eab361f666f5f23a04806ffb
Shy Agam <us@bespunky.io>
2021-02-16 20:45:18 +0200

    📃 Documented providers

--- projects/bespunky/angular-timeline/skeleton/services/render/timeline-renderer.provider.ts
+++ projects/bespunky/angular-timeline/skeleton/services/render/timeline-renderer.provider.ts
@@ -2,6 +2,11 @@ import { ClassProvider } from '@angular/core';
 import { TimelineRenderer } from './timeline-renderer';
 import { TimelineRendererService } from './timeline-renderer.service';
 
+/**
+ * Provides the default implementation for the `TimelineRenderer` class.
+ *
+ * Provided by the timeline directive.
+ */
 export const TimelineRendererProvider: ClassProvider = {
     provide : TimelineRenderer,
     useClass: TimelineRendererService
--- projects/bespunky/angular-timeline/skeleton/services/state/timeline-state.provider.ts
+++ projects/bespunky/angular-timeline/skeleton/services/state/timeline-state.provider.ts
@@ -2,6 +2,11 @@ import { ClassProvider } from '@angular/core';
 import { TimelineState } from './timeline-state';
 import { TimelineStateService } from './timeline-state.service';
 
+/**
+ * Provides the default implementation for the `TimelineTickRender` class.
+ *
+ * Provided by the timeline directive.
+ */
 export const TimelineStateProvider: ClassProvider = {
     provide : TimelineState,
     useClass: TimelineStateService
