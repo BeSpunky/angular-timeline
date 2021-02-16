f345967e1905c3453a47787fc07d6fb117f33478
Shy Agam <us@bespunky.io>
2021-02-16 17:01:44 +0200

    📃 Documented the tick virtualization service

--- projects/bespunky/angular-timeline/skeleton/modules/ticks/services/virtualization/timeline-tick-virtualization.service.ts
+++ projects/bespunky/angular-timeline/skeleton/modules/ticks/services/virtualization/timeline-tick-virtualization.service.ts
@@ -7,11 +7,25 @@ import { DatesBetweenGenerator, DayFactor, TickLabeler } from '../../view-models
 import { distinctUntilChanged, map, observeOn } from 'rxjs/operators';
 import { TimelineTick } from '../../directives/timeline-tick';
 
+/**
+ * Provides methods for virtualizing tick rendering. This service is designed to determine what ticks should
+ * be displayed on the screen according to screen size, zoom, view center and other factors.
+ *
+ * @export
+ * @class TimelineTickVirtualizationService
+ */
 @Injectable({ providedIn: 'root' })
 export class TimelineTickVirtualizationService
 {
     constructor(private location: TimelineLocationService) { }
 
+    /**
+     * Creates a stream that notifies subscribers when the specified tick scale should render or unrender.
+     * Render state is determined by the current zoom level and the min/max zoom defined for the tick scale.
+     *
+     * @param {TimelineTick} tick The tick scale for which to create the stream.
+     * @returns {Observable<boolean>} An observable that notifies subscribers when to render and unrender the tick.
+     */
     public shouldRenderFeed(tick: TimelineTick): Observable<boolean>
     {
         return combineLatest([tick.state.zoom, tick.minZoom, tick.maxZoom]).pipe(
@@ -20,6 +34,19 @@ export class TimelineTickVirtualizationService
         );
     }
 
+    /**
+     * Creates a stream that notifies subscribers when the ticks that should be displayed on the screen have changed.
+     * A new item array will be generated any time one of the following occurs:
+     * - The tick label function has been replaced
+     * - The tick dateBetween function has been replaced
+     * - The tick dayFactor has been updated
+     * - The timeline zoom level has changed
+     * - The timeline view center has changed
+     * - The timeline ticks buffer size has changed
+     *
+     * @param {TimelineTick} tick The tick scale for which to create the stream.
+     * @returns {Observable<TickItem[]>} A stream that notifies subscribers when the ticks that should be displayed on the screen have changed.
+     */
     public itemsToRenderFeed(tick: TimelineTick): Observable<TickItem[]>
     {
         return combineLatest([tick.label, tick.datesBetween, tick.dayFactor, tick.state.dayWidth, tick.state.viewBounds, tick.state.ticksBuffer]).pipe(
@@ -40,15 +67,30 @@ export class TimelineTickVirtualizationService
         );
     }
 
+    /**
+     * Generates an array of tick items representing the ticks that should be displayed on the screen given
+     * the specified state.
+     *
+     * @param {number} dayWidth The width (in pixels) representing one single day on the timeline.
+     * @param {DayFactor} dayFactor The multiplier specifying what part of a single day (or how many days) this tick scale represents.
+     * @param {number} startPosition The start position (in pixels) from which ticks should start. This should include any buffer width.
+     * @param {number} endPosition The end position (in pixels) from which ticks should end. This should include any buffer width.
+     * @param {DatesBetweenGenerator} datesBetween The function that generates all tick scale-level dates between two given dates.
+     * @param {TickLabeler} label The function to use for labeling the items.
+     * @returns {TickItem[]} An array of tick items representing the ticks that should be displayed on the screen.
+     */
     public ticksOnScreen(dayWidth: number, dayFactor: DayFactor, startPosition: number, endPosition: number, datesBetween: DatesBetweenGenerator, label: TickLabeler): TickItem[]
     {
+        // Create the function that will generate the width of the ticks
         const width = dayFactor instanceof Function
             ? (date: Date) => dayFactor(date) * dayWidth
             : (          ) => dayWidth;
         
+        // Find the dates corresponding to the bounds of the screen
         const start = this.location.positionToDate(dayWidth, startPosition);
         const end   = this.location.positionToDate(dayWidth, endPosition);
 
+        // Generate all scale-level dates inside the screen bounds and create a tick item for each
         return datesBetween(start, end).map(date => new TickItem(this.location.dateToPosition(dayWidth, date), date, width(date), label(date)));
     };
 }
