7f86e431d49a8cb09dfca86822c9e6d613b0ebfb
Shy Agam <us@bespunky.io>
2021-02-19 06:16:43 +0200

    ➕ Implemented visualization of the current date

--- projects/bespunky/angular-timeline/skeleton/directives/timeline.directive.ts
+++ projects/bespunky/angular-timeline/skeleton/directives/timeline.directive.ts
@@ -1,6 +1,6 @@
 import { AfterViewInit, ChangeDetectorRef, ContentChildren, Directive, Input, QueryList } from '@angular/core';
 import { Destroyable } from '@bespunky/angular-zen/core';
-import { Observable } from 'rxjs';
+import { combineLatest, Observable } from 'rxjs';
 import { filter, map, startWith, takeUntil } from 'rxjs/operators';
 import { TimelineState } from '../services/state/timeline-state';
 import { TimelineStateProvider } from '../services/state/timeline-state.provider';
@@ -12,6 +12,8 @@ import { TimelineTickDirective } from '../modules/ticks/directives/timeline-tick
 import { TimelineTick } from '../modules/ticks/directives/timeline-tick';
 import { TimelineTickRendererService } from '../modules/ticks/services/render/timeline-tick-renderer.service';
 import { TimelineCameraProvider } from '../services/camera/timeline-camera.provider';
+import { TimelineCamera } from '../services/camera/timeline-camera';
+import { TimelineLocationService } from '../services/location/timeline-location.service';
 
 /**
  * Adds timeline functionality to an element.
@@ -35,7 +37,8 @@ export class TimelineDirective extends Destroyable implements AfterViewInit
      *
      * @type {Observable<string>}
      */
-    public readonly svgViewBox: Observable<string>;
+    public readonly svgViewBox : Observable<string>;
+    public readonly currentDate: Observable<Date>;
     
     /**
      * Creates an instance of TimelineDirective.
@@ -50,6 +53,8 @@ export class TimelineDirective extends Destroyable implements AfterViewInit
         private changes     : ChangeDetectorRef,
         public  state       : TimelineState,
         private control     : TimelineControl,
+        private camera      : TimelineCamera,
+        private location    : TimelineLocationService,
         private renderer    : TimelineRenderer,
         private tickRenderer: TimelineTickRendererService
     )
@@ -57,11 +62,17 @@ export class TimelineDirective extends Destroyable implements AfterViewInit
         super();
 
         this.svgViewBox = this.state.viewBounds.pipe(map(viewBounds => viewBounds.toSvgViewBox()));
+
+        this.currentDate = combineLatest([this.state.dayWidth, this.state.viewCenter]).pipe(
+            map(([dayWidth, viewCenter]) => this.location.positionToDate(dayWidth, viewCenter))
+        );
     }
 
     ngAfterViewInit()
     {
         this.observeTicks();
+
+        this.camera.moveTo(new Date());
 
         this.changes.detectChanges();
     }
