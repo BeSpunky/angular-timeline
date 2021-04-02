d042829d0e0552422395a2017fa24846ccf97e61
Shy Agam <us@bespunky.io>
2021-04-02 20:11:32 +0300

    ✨ Generated a core entrypoint for the timeline directive

--- libs/angular-timeline/index.ts
+++ libs/angular-timeline/index.ts
@@ -1,3 +1,4 @@
+export * from '@bespunky/angular-timeline/core';
 export * from '@bespunky/angular-timeline/state';
 export * from '@bespunky/angular-timeline/control';
 export * from '@bespunky/angular-timeline/render';
