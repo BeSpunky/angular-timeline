68b1cd8cf586389f842335ca9a5d61c23c23ace5
Shy Agam <us@bespunky.io>
2021-04-02 20:09:21 +0300

    ✨ Generated entrypoints for the different operation modules
    Decided that the timeline package will not have a default design and will only provide operation tools.

--- libs/angular-timeline/index.ts
+++ libs/angular-timeline/index.ts
@@ -1,1 +1,4 @@
-export * from '@bespunky/angular-timeline/operation';
+export * from '@bespunky/angular-timeline/state';
+export * from '@bespunky/angular-timeline/control';
+export * from '@bespunky/angular-timeline/render';
+export * from '@bespunky/angular-timeline/ticks';
