fd9f55cdb09d45c8fd98ea1ae2edfe49e61cf737
Shy Agam <us@bespunky.io>
2021-04-04 21:01:05 +0300

    ♻ Adapted the demo app stories file to new storybook version
    Seems like the schematics created an outdated stories file

--- apps/demo/src/app/app.component.stories.ts
+++ apps/demo/src/app/app.component.stories.ts
@@ -1,15 +1,20 @@
 
+import { moduleMetadata } from '@storybook/angular';
 import { AppComponent } from './app.component';
+import { AppTimelineModule } from './timeline/timeline.module';
 
 export default {
-  title: 'AppComponent'
+    title    : 'AppComponent',
+    component: AppComponent,
+    decorators: [
+        moduleMetadata({
+            declarations: [AppComponent],
+            imports     : [AppTimelineModule],
+        })
+    ]
 }
 
 export const primary = () => ({
-  moduleMetadata: {
-    imports: []
-  },
-  component: AppComponent,
   props: {
   }
 })
