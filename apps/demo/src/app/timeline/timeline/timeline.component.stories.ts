129c37dc03bc999004ff2173c48a90539a594bcd
Shy Agam <us@bespunky.io>
2021-04-06 00:12:18 +0300

    📖 Added stories for new timeline quick configs

--- apps/demo/src/app/timeline/timeline/timeline.component.stories.ts
+++ apps/demo/src/app/timeline/timeline/timeline.component.stories.ts
@@ -4,18 +4,25 @@ import { TimelineModule    } from '@bespunky/angular-timeline/core';
 import { TimelineComponent } from './timeline.component';
 
 export default {
-    title: 'TimelineComponent',
+    title     : 'TimelineComponent',
     decorators: [
         moduleMetadata({
             declarations: [TimelineComponent],
-            imports: [TimelineModule],
-        }),
+            imports     : [TimelineModule]
+        })
     ],
     component: TimelineComponent,
     argTypes: {
-        date        : { name: 'Date'          , defaultValue: new Date(), control: { type: 'date'   } },
-        zoom        : { name: 'Zoom Level'    , defaultValue: 0         , control: { type: 'range', min: -250, max: 250 } },
-        baseTickSize: { name: 'Base Tick Size', defaultValue: 1         , control: { type: 'range', min: 0.01 } },
+        date                : { name: 'Date'                   , defaultValue: new Date(), control: { type: 'date'   } },
+        zoom                : { name: 'Zoom Level'             , defaultValue: 0         , control: { type: 'range', min: -250, max: 250 } },
+        baseTickSize        : { name: 'Base Tick Size'         , defaultValue: 1         , control: { type: 'range', min: 0.01 } },
+        moveAmount          : { name: 'Move Amount'            , defaultValue: 1         , control: { type: 'range', min: 1, max: 10, step: 0.5 }},
+        moveOnKeyboard      : { name: 'Move On Keyboard'       , defaultValue: true      , control: { type: 'boolean' }},
+        moveOnWheel         : { name: 'Move On Wheel'          , defaultValue: true      , control: { type: 'boolean' }},
+        virtualizationBuffer: { name: 'Virtualization Buffer'  , defaultValue: 0.5       , control: { type: 'range', min: 0.1, max: 3, step: 0.1 }},
+        zoomDeltaFactor     : { name: 'Zoom Delta Factor'      , defaultValue: 1.06      , control: { type: 'range', min: 1.01, max: 3, step: 0.01 }},
+        zoomOnKeyboard      : { name: 'Zoom On Keyboard'       , defaultValue: true      , control: { type: 'boolean' }},
+        zoomOnWheel         : { name: 'Zoom On Wheel'          , defaultValue: true      , control: { type: 'boolean' }}
     },
 } as Meta;
 
@@ -39,3 +46,15 @@ ZoomIn.args = { zoom: 100 };
 export const ZoomOut = BaseStory.bind({});
 ZoomOut.storyName = 'Zoom Out';
 ZoomOut.args = { zoom: -60 };
+
+export const MouseDisabled = BaseStory.bind({});
+MouseDisabled.storyName = 'Mouse Disabled';
+MouseDisabled.args = { moveOnWheel: false, zoomOnWheel: false };
+
+export const KeyboardDisabled = BaseStory.bind({});
+KeyboardDisabled.storyName = 'Keyboard Disabled';
+KeyboardDisabled.args = { moveOnKeyboard: false, zoomOnKeyboard: false };
+
+export const ExtraLargeVirtualizationBuffer = BaseStory.bind({});
+ExtraLargeVirtualizationBuffer.storyName = 'Extra Large Virtualization Buffer';
+ExtraLargeVirtualizationBuffer.args = { virtualizationBuffer: 5 };
